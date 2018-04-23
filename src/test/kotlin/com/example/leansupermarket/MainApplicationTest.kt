f35c409d63afad99df30e3f27cb512da3b412baa
Alvaro Garcia <derecerca@gmail.com>
2018-04-24 00:30:55 +0200

    Red: Acceptance for Iteration 1 - get a discount for cherries

--- src/test/kotlin/com/example/leansupermarket/MainApplicationTest.kt
+++ src/test/kotlin/com/example/leansupermarket/MainApplicationTest.kt
@@ -1,28 +1,22 @@
 package com.example.leansupermarket
 
 import com.example.kata.leansupermarket.CheckoutCalculator
-import com.example.kata.leansupermarket.main
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class MainApplicationTest {
-    @Test
-    fun `make use of the main application`() {
-        main(arrayOf("0", "1,", "2"))
-    }
-
     @Test
     fun `test apples`() {
         assertThat(CheckoutCalculator().ring("Apples").total()).isEqualTo(100)
     }
 
     @Test
-    fun `Iteration 0 - Acceptance test`() {
+    fun `Iteration 1 - Acceptance test`() {
         assertThat(CheckoutCalculator()
                 .ring("Apples")
                 .ring("Cherries")
                 .ring("Cherries")
-                .total()).isEqualTo(250)
+                .total()).isEqualTo(250 - 20)
     }
 
 }
