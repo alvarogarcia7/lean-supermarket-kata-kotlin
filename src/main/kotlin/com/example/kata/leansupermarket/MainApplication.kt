e976b1d2640558139a64e243288dac7a0b3664de
Alvaro Garcia <derecerca@gmail.com>
2018-04-24 00:29:08 +0200

    Create a main Runner to execute from the console

--- src/main/kotlin/com/example/kata/leansupermarket/MainApplication.kt
+++ src/main/kotlin/com/example/kata/leansupermarket/MainApplication.kt
@@ -1,5 +1,23 @@
 package com.example.kata.leansupermarket
 
+import java.util.*
+
 fun main(args: Array<String>) {
-    args.forEach { println(it) }
+    val reader = Scanner(System.`in`)
+    var lines = mutableListOf<String>()
+    while (true) {
+        val nextLine = reader.nextLine()
+        if (nextLine.isEmpty()) {
+            break
+        } else {
+            lines.add(nextLine)
+        }
+    }
+    checkout(lines)
+}
+
+private fun checkout(lines: List<String>) {
+    val calculator = CheckoutCalculator()
+    lines.forEach { calculator.ring(it) }
+    println(calculator.total)
 }
