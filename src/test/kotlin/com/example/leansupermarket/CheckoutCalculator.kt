39456deaa2c625733f10e2085558263bd0869ee7
Alvaro Garcia <derecerca@gmail.com>
2018-04-24 00:21:07 +0200

    green

--- src/test/kotlin/com/example/leansupermarket/CheckoutCalculator.kt
+++ src/test/kotlin/com/example/leansupermarket/CheckoutCalculator.kt
@@ -1,12 +1,14 @@
 package com.example.leansupermarket
 
 class CheckoutCalculator {
+    var total = 0
     fun ring(product: String): CheckoutCalculator {
+        total += 100
         return this
     }
 
     fun total(): Int {
-        return 0
+        return total
     }
 
 }
